14fb39da4ac2ba2c43243be7b69001f99a041088
Max Strange <maxfieldstrange@gmail.com>
2022-10-31 11:45:33 -0700

    WIP: Autoencoder

--- clustering/networks/autoencoder.py
+++ clustering/networks/autoencoder.py
@@ -4,25 +4,30 @@ import logging
 def _make_original_network(config):
     embedding_dims = config.getint('Network', 'embedding-dims')
     encoder = nn.Sequential(
-        nn.Conv2d(1, 2, (5, 5), stride=(2, 1)),
+        nn.Conv2d(1, 2, (3, 3), stride=(2, 1)),         # 1
         nn.ReLU(),
-        nn.Conv2d(2, 4, (5, 5), stride=(2, 2)),
+        nn.Conv2d(2, 4, (3, 3), stride=(2, 2)),         # 2
         nn.ReLU(),
-        nn.Conv2d(4, 8, (5, 5), stride=(2, 2)),
+        nn.Conv2d(4, 8, (3, 3), stride=(2, 2)),         # 3
         nn.BatchNorm2d(8),
         nn.ReLU(),
-        nn.Conv2d(8, 16, (5, 5), stride=(2, 2)),
+        nn.Conv2d(8, 16, (3, 3), stride=(2, 2)),        # 4
         nn.ReLU(),
-        nn.Conv2d(16, 32, (4, 4)),
+        nn.Conv2d(16, 32, (3, 3)),                      # 5
         nn.ReLU(),
-        nn.Conv2d(32, 64, (3, 3)),
+        nn.Conv2d(32, 64, (3, 3)),                      # 6
         nn.BatchNorm2d(64),
         nn.ReLU(),
-        nn.Conv2d(64, 128, (2, 3)),
+        nn.Conv2d(64, 128, (3, 3)),                     # 7
         nn.ReLU(),
-        nn.Conv2d(128, 256, (2, 3)),
+        nn.Conv2d(128, 256, (2, 3)),                    # 8
         nn.ReLU(),
-        nn.Conv2d(256, 512, (2, 2)),
+        nn.Conv2d(256, 512, (2, 3)),                    # 9
+        nn.BatchNorm2d(512),
+        nn.ReLU(),
+        nn.Conv2d(512, 512, (2, 2)),                    # 10
+        nn.ReLU(),
+        nn.Conv2d(512, 512, (2, 2)),                    # 11
         nn.BatchNorm2d(512),
         nn.ReLU(),
         nn.Flatten(),
@@ -33,25 +38,30 @@ def _make_original_network(config):
         nn.Linear(embedding_dims, 512),
         nn.ReLU(),
         nn.Unflatten(1, (512, 1, 1)),
-        nn.ConvTranspose2d(512, 512, (2, 2)),
+        nn.ConvTranspose2d(512, 512, (2, 2)),                                               # 11
         nn.ReLU(),
-        nn.ConvTranspose2d(512, 512, (2, 3)),
-        nn.ReLU(),
-        nn.ConvTranspose2d(512, 512, (2, 3)),
+        nn.ConvTranspose2d(512, 512, (2, 2)),                                               # 10
         nn.BatchNorm2d(512),
         nn.ReLU(),
-        nn.ConvTranspose2d(512, 256, (3, 3)),
+        nn.ConvTranspose2d(512, 512, (2, 3)),                                               # 9
         nn.ReLU(),
-        nn.ConvTranspose2d(256, 256, (4, 4)),
+        nn.ConvTranspose2d(512, 512, (2, 3)),                                               # 8
         nn.ReLU(),
-        nn.ConvTranspose2d(256, 128, (5, 5), stride=(2, 2), output_padding=(1, 0)),
+        nn.ConvTranspose2d(512, 512, (3, 3)),                                               # 7
+        nn.BatchNorm2d(512),
+        nn.ReLU(),
+        nn.ConvTranspose2d(512, 256, (3, 3)),                                               # 6
+        nn.ReLU(),
+        nn.ConvTranspose2d(256, 256, (3, 3)),                                               # 5
+        nn.ReLU(),
+        nn.ConvTranspose2d(256, 128, (3, 3), stride=(2, 2), output_padding=(1, 0)),         # 4
         nn.BatchNorm2d(128),
         nn.ReLU(),
-        nn.ConvTranspose2d(128, 64, (5, 5), stride=(2, 2), output_padding=(1, 0)),
+        nn.ConvTranspose2d(128, 64, (3, 3), stride=(2, 2), output_padding=(1, 0)),          # 3
         nn.ReLU(),
-        nn.ConvTranspose2d(64, 16, (5, 5), stride=(2, 2)),
+        nn.ConvTranspose2d(64, 16, (3, 3), stride=(2, 2)),                                  # 2
         nn.ReLU(),
-        nn.ConvTranspose2d(16, 1, (5, 5), stride=(2, 1)),
+        nn.ConvTranspose2d(16, 1, (3, 3), stride=(2, 1)),                                   # 1
         nn.Sigmoid()
         # TODO: Use smaller kernels and/or use stride later in the encoder/earlier in the decoder, that way
         #       we hopefully will be able to get the bottom and the top? <-- Think about this
