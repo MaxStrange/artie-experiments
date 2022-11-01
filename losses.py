0854610ef486a1f2b0c876c57affbb508096be3a
Max Strange <maxfieldstrange@gmail.com>
2022-11-01 16:52:48 -0700

    WIP: Fix reconstructive loss

--- losses.py
+++ losses.py
@@ -12,19 +12,14 @@ def reconstruction_loss(pred_batch, label_batch):
     valued pixels in the spectrograms.
     """
     mask_zeros_in_label = torch.isclose(label_batch, torch.zeros_like(label_batch)).int()
-    batch_sums_of_num_zeros_in_label = torch.sum(mask_zeros_in_label, (1, 2), dtype=label_batch.dtype)
+    batch_sums_of_num_zeros_in_label = torch.sum(mask_zeros_in_label, (1, 2, 3), dtype=label_batch.dtype)
     mask_nonzeros_in_label = (label_batch > 0.0).int()
-    batch_sums_of_num_non_zeros_in_label = torch.sum(mask_nonzeros_in_label, (1, 2), dtype=label_batch.dtype)
+    batch_sums_of_num_non_zeros_in_label = torch.sum(mask_nonzeros_in_label, (1, 2, 3), dtype=label_batch.dtype)
     batch_balance = batch_sums_of_num_zeros_in_label / batch_sums_of_num_non_zeros_in_label
     balance = torch.mean(batch_balance, dtype=label_batch.dtype)
     balances = torch.ones_like(label_batch, dtype=label_batch.dtype)
     balances[mask_nonzeros_in_label.bool()] = balance
-
-    difference = nn.functional.l1_loss(pred_batch, label_batch)
-
-    # diff[mask_nonzeros_in_label] should get multiplied by the appropriate balanceing factor
-    balanced_difference = difference * balances
-    return torch.mean(balanced_difference)
+    return torch.mean(torch.abs(pred_batch - label_batch) * balances)
 
 def make_loss_function_from_config_file(config: configuration.Configuration):
     """
@@ -42,7 +37,8 @@ def make_loss_function_from_config_file(config: configuration.Configuration):
             raise ValueError(errmsg)
 
 if __name__ == "__main__":
-    pred_batch = torch.rand((2, 10, 5), dtype=torch.float32, requires_grad=True)
+    pred_batch = torch.rand((2, 1, 10, 5), dtype=torch.float32, requires_grad=True)
     label_batch = torch.zeros_like(pred_batch, dtype=torch.float32)
-    label_batch[:, 4, :] = 1.0
+    label_batch[:, :, 4, :] = 1.0
     loss = reconstruction_loss(pred_batch, label_batch)
+    print(loss, torch.nn.functional.l1_loss(pred_batch, label_batch))
